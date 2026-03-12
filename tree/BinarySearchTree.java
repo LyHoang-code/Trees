3cc61d83e81fe9ad07fc9fbed96d53015e4bb9f8
LyHoang-code <lhoang1971739396@gmail.com>
2026-03-12 02:33:27 +0000

    update BST and Node

--- tree/BinarySearchTree.java
+++ tree/BinarySearchTree.java
@@ -1,5 +1,121 @@
 package tree;
 
+import java.util.Collection;
+
+/** Remember Search, add, remove time complexity is Olog(N)
+*/ 
+
 public class BinarySearchTree<T extends Comparable < ? super T>> {
-    private 
+    private Node<T> rootNode;
+    private int size;
+
+    public BinarySearchTree() {
+    }
+
+    /**
+     * Guide  Not all Collectionss are 
+     * indexable like but all collections are Iterable
+     * 
+     * -> using enhance loop
+     * */
+    public BinarySearchTree(Collection<T> data) {
+        if (data == null) {
+            throw new IllegalArgumentException("the passed in data is null");
+        }
+        for (T elements : data) {
+            if (elements == null) {
+                throw new IllegalArgumentException();
+            }
+            //add method goes here.
+            add(elements);
+        }
+        
+   }
+ /**
+ * require to use recursive to do the add method
+ * 
+ * @param elements
+ */
+    public void add(T elements) {
+       rootNode = recursiveAdd(rootNode,elements);
+    }
+
+    private Node<T> recursiveAdd(Node<T> rootNode, T elements) {
+    // situation where the tree is empty and we add the first node
+    if(rootNode == null) {
+        size++;
+        //basically we just set the root to the elements
+        Node<T> node = new Node<>(elements);
+        return node;
+    }
+    //situation where the tree is not empty and we have to add to left or right
+    if (elements.compareTo(rootNode.getData()) < 0) {
+        rootNode.setLeft(recursiveAdd(rootNode.getLeftNode(), elements));
+    }
+    if (elements.compareTo(rootNode.getData()) > 0) {
+        rootNode.setRight(recursiveAdd(rootNode.getRightNode(), elements));
+    }
+    return rootNode;
+ }
+
+ /**
+  * for remove do both predecessor and successor
+  * have to be recursively
+  */
+    public T remove(T element) {
+        Node<T> temp = new Node<>(null);
+        rootNode =   recursiveRemove(element, rootNode, temp);
+        return temp.getData();
+    }
+
+ private Node<T> recursiveRemove(T element, Node<T> rootNode, Node<T> temp) {
+    if (rootNode == null) {
+        return null;
+    } else if (element.compareTo(rootNode.getData()) < 0) {
+        rootNode.setLeft(recursiveRemove(element, rootNode.getLeftNode(), temp));
+    } else if (element.compareTo(rootNode.getData()) > 0) {
+        rootNode.setRight(recursiveRemove(element, rootNode.getRightNode(), temp));
+    } else {
+        temp.setData(rootNode.getData());
+        size--;
+
+        if (rootNode.getLeftNode() == null && rootNode.getRightNode() == null) {
+
+            return null;
+
+        } else if (rootNode.getLeftNode() == null) {
+
+            return rootNode.getRightNode();
+
+        } else if (rootNode.getRightNode() == null) {
+
+            return rootNode.getLeftNode();
+
+        } else {
+            Node<T> secondTemp = new Node<>(null);
+            //if use predecessor
+            rootNode.setLeft(predecessor(rootNode.getLeftNode(),secondTemp));
+            rootNode.setData(secondTemp.getData());
+        }
+    }
+ }
+
+ private Node<T> predecessor(Node<T> leftNode, Node<T> secondTemp) {
+    if (leftNode.getRightNode() == null) {
+        secondTemp.setData(leftNode.getData());
+        return leftNode.getLeftNode();
+    } else {
+        leftNode.setRight(predecessor(leftNode.getRightNode(), secondTemp));
+    }
+    return leftNode;
+ }
+
+
+
+
+
+
+
+
+
 }
--- tree/Node.java
+++ tree/Node.java
@@ -1,12 +1,51 @@
 package tree;
 
-public class Node<T extends Comparable <? super T>> {
+ class Node<T extends Comparable <? super T>> {
     
     private T data; 
     private Node<T> left;
     private Node<T> right;
-    private int height;
+    
+    /**
+     * Constructs a BSTNode with the given data
+     * 
+     * @param data
+     */
+    Node (T data) { this.data = data; }
 
+    /**
+     * Gets the data
+     * @return the data
+     */
 
+    T getData(){ return data; }
+
+    /**
+     * Gets the left child
+     * 
+     * @return the left child
+     */
+    Node<T> getLeftNode() { return left; }
+
+    /**
+     * Gets the right child
+     * 
+     * @return the right child
+     */
+    Node<T> getRightNode() { return right; }
+    
+    /**
+     * Sets the data
+     * 
+     * @param data
+     */
+    void setData(T data) { this.data = data;}
+
+    void setLeft(Node<T> left) { this.left = left; }
+
+    void setRight(Node<T> right) { this.right = right; }
+
+    @Override 
+    public String toString() { return "Node containing: " + data; }
 
 }
